20ff33f0b41d001d8ee97dbb842338e3ba3f89e2
Ben Walandow <bentol777@yahoo.com>
2020-09-13 19:49:37 +0700

    Update Helper

--- src/helper.php
+++ src/helper.php
@@ -15,6 +15,39 @@ if(!function_exists("locale")) {
     }
 }
 
+
+/**
+ * Get the path to a versioned Mix file.
+ *
+ * @param  string  $path
+ * @param  string  $manifestDirectory
+ * @return \Illuminate\Support\HtmlString|string
+ *
+ * @throws \Exception
+ */
+if (! function_exists('asset_path')) {
+    function asset_path($path, $mixin = false)
+    {
+
+        $env     = env('APP_ENV');
+        $cdnUrl  = env('CDN_URL');
+
+        if($mixin) {
+            $path = mix($path);
+            $path = substr($path,1);
+        }
+
+        // Reference CDN assets only in production or staging environemnt.
+        // In other environments, we should reference locally built assets.
+        if ($cdnUrl && ($env === 'production')) {
+            $mixPath = $cdnUrl . $path;
+        } else {
+            $mixPath = asset($path);
+        }
+        return $mixPath;
+    }
+}
+
 /**
  * @param string $string
  * @param string $endString
@@ -72,13 +105,23 @@ if (!function_exists('el_url')) {
             if ($check[0] === "/" || $check[0] === "\\") {
                 $check = substr($check, 1);
             }
-            return url($check);
+            return env('CDN_URL') != '' ? asset_path($check) : url($check);
         } else {
             return '';
         }
     }
 }
 
+/**
+ * Check CMS Element Exists or Not,
+ * then return CDN Full URL
+ *
+ * @param array $page
+ *
+ * @param string $key
+ *
+ * @return string
+ */
 if (!function_exists('el_cdn_url')) {
     function el_cdn_url($page, $key)
     {
@@ -160,6 +203,30 @@ if(!function_exists('phoneval')) {
         preg_match_all('!\d+!', $str, $matches);
         $var = implode('', $matches[0]);
         return "+".$var;
+    }
+}
+
+/**
+ * Dial Phone Helper
+ *
+ * Any Number format into +62
+ * @param string $key
+ *
+ * @param string $country_code
+ *
+ * @return string
+ */
+if(!function_exists('phoneDial')) {
+    function phoneDial($number_text, $country_code = '+62')
+    {
+        preg_match_all('!\d+!', $number_text, $matches);
+        $matches = implode('', $matches[0]);
+        if($matches[0] === '0') {
+            $matches = $country_code.substr($matches,1);
+        } else {
+            $matches = $country_code.$matches;
+        }
+        return $matches;
     }
 }
 
